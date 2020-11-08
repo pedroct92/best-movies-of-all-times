8215425fa8f06f78937688711c95a09f8579bf2d
Pedro Coelho Torres <pedro.torres@intelerad.com>
2020-11-07 21:14:28 -0500

    update tasks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # best-movies-of-all-times
 Angular + NgRX + Angular Material
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/8ec455cb-8d1c-497c-8438-b6ac51d058e4/deploy-status)](https://app.netlify.com/sites/thirsty-shannon-6b9332/deploys)
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('bmoat');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bmoat app is running!');
-  });
 });
